8314c6369e1362b77cd3e439f1707d75c79c4f2f
Kevin-CodeCrew <kevin@code-crew.org>
2018-11-19 07:50:34 -0600

    Added basic layout

--- app/src/main/java/org/code_crew/calculator/MainActivity.java
+++ app/src/main/java/org/code_crew/calculator/MainActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // Define variables to use for input and calculations
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add any code to handle the buttons
     }
+
+
+    // Add method(s) to handle numbers
+
+    // Add method(s) to handle operations
+
 }
